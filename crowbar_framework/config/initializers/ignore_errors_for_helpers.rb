92600293e3d7106cba11ddbff3820b6500be5a06
Evgeniy Shurmin <eshurmin@gmai.com>
2013-02-12 16:25:05 +0400

    ignore errors with loading relative helpers of controller

---
+++ crowbar_framework/config/initializers/ignore_errors_for_helpers.rb
@@ -0,0 +1,3 @@
+# ignore errors with loading relative helpers of controller
+# this errors appear when using ruby 1.9.1 
+MissingSourceFile::REGEXPS << [/^cannot load such file -- \w+_helper (.+)$/i, 1]
