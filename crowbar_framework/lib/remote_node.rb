2e1bd0e3debdb5d8ccb7f39ac802f8025e54a3e3
Sergey V. Yudin <tsipa740@gmail.com>
2013-08-26 11:47:35 +0000

    some debug info

--- crowbar_framework/lib/remote_node.rb
+++ crowbar_framework/lib/remote_node.rb
@@ -7,16 +7,21 @@ module RemoteNode
     sleep(10)
     nobj = NodeObject.find_node_by_name(host)
     start = Time.now.to_i
+    puts "starting main loop"
     while (Time.now.to_i - start) < timeout do
       begin
+        puts "checking socket"
         TCPSocket.new(host, 22)
         if %w(redhat centos).include?(nobj[:platform])
+          puts "checking ssh for rhel"
           raise "next" unless system("sudo -i -u root -- ssh root@#{host} 'runlevel | grep \"N 3\"'")
         else
+          puts "checking ssh non-rhel"
           raise "next" unless system("sudo -i -u root -- ssh root@#{host} 'last | head -n1 | grep -vq down'")
         end
         return true
       rescue
+        puts "sleeping until next cycle"
         sleep(10)
         next
       end
